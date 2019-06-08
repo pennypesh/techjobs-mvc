006bfb6f63436d1230e426e6aaf621e9e41f31c6
pennypesh <pennydehoney@yahoo.com>
2019-06-08 14:26:27 -0500

    initial commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,20 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model,@RequestParam String searchType,@RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> result=JobData.findByColumnAndValue(searchType,searchTerm);
+        System.out.println("JOBBBBBBB!! "+result.size());
+
+        for(HashMap<String, String> list: result){
+            System.out.println("TTTTTTTTT"+list.entrySet());;
+
+        }
+
+        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("results",result);
+        return "search";
+    }
 
 }
